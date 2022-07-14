7e6d6f1e839cc6afc3af7fac1cd8303bf5d82579
Rocío Mena <rociocmena@gmail.com>
2022-07-13 21:49:07 -0300

    Change params

--- code/cube.py
+++ code/cube.py
@@ -123,14 +123,13 @@ class RubikCube:
         for face in faces:
             for i in range(face.piecesList.length):
                 tile = face.piecesList.array[i]
-                # Add one point if the tile is in the correct face.
                 if (tile.color == face.color):
-                    score += 1
-                    # Add one extra point if the tile is in the correct position of the correct face.
+                    # Add one point if the tile is in the correct position of the correct face.
                     if (int(tile.id) == i):
-                        score += 1
-        if (len(self.history) >= 20 and len(self.history) <= 70):
-            score += 100//len(self.history)
+                        if (int(tile.id) in [1,3,5,7]):
+                            score += 1.5 #More score for edges.
+                        else:
+                            score += 0.5 #Less score for vertex.
         return score
 
     # Rotate a given face and the side pieces in the side faces.
--- code/genetic.py
+++ code/genetic.py
@@ -8,7 +8,7 @@ class GeneticAlgorithm:
     initialHistory = None
     result = None
     time = 0
-    initialStateMovements = 30
+    initialStateMovements = 40
     populationSize = 150
     maxTime = 500
     mutationProbability = 0.2
